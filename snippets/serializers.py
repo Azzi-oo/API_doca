48c66573b73af796e0aa0ed3ae3b03bf935b6dca
Azzi-oo <yanberdin.az@yandex.ru>
2023-12-12 15:28:18 +0500

    add new changes

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,16 +3,19 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
-    owner = serializers.ReadOnlyField(source='owner.username')
+class UserSerializer(serializers.
+                     HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedIdentityField(many=True, view_name='snippet-highlight', format='html')
 
     class Meta:
         model = User
         fields = ['id', 'username', 'snippets']
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
     class Meta:
         model = Snippet
         fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
